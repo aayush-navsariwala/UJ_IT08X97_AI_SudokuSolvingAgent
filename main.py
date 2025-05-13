0949f89cd4e3d78f3c83d32ff11042cf97f8e5ba
aayush-navsariwala <aayush.nav@gmail.com>
2025-05-13 21:15:51 +0200

    Commit 4 - Working backtracking algorithm with hardcoded 9x9 board

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
-import core.board
-from gui.sudoku_gui import launch_gui
+from core.board import SudokuBoard
 from core.fsm import FSMManager
+from core.states import InputState
+from solver.backtracking import solve
 
 if __name__ == "__main__":
-    board = core.board.SudokuBoard()
+    # Sample incomplete Sudoku puzzle (0 implies the field is empty)
+    sample_grid = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    # Initializing the Sudoku board
+    board = SudokuBoard(sample_grid)
+    print("Initial Sudoku Board:")
+    board.display()
+
+    # Initializing the FSM
     fsm = FSMManager()
-    launch_gui(board, fsm)
+    fsm.set_state(InputState())
+    print("\nRunning FSM:")
+    fsm.update()
+    
+    # Run Backtracking Solver
+    print("\nSolving Sudoku with Backtracking:")
+    if solve(board):
+        print("Solved Sudoku Board:")
+        board.display()
+    else:
+        print("No solution found.")
