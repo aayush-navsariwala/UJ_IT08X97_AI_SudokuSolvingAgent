f9745cb1910371151f1cf75ab84f352b2e28b7ce
aayush-navsariwala <aayush.nav@gmail.com>
2025-05-19 00:31:54 +0200

    Commit 31 - Added comments to board.py and fsm.py

--- core/board.py
+++ core/board.py
@@ -1,28 +1,35 @@
 class SudokuBoard:
     def __init__(self, grid=None):
+        # Initialise the sudoku board
+        # If not grid is given, create a 9x9 grid filled with empty cells
         self.grid = grid if grid else [[0 for _ in range(9)] for _ in range(9)]
 
+    # Check if placing a number at (row, col) is valid according to the game rules
     def is_valid(self, row, col, num):
-        # Row check
+        
+        # Checks to see if the number is already in the same row 
         if num in self.grid[row]:
             return False
         
-        # Column check
+        # Checks to see if the number is already in the same column
         for i in range(9):
             if self.grid[i][col] == num:
                 return False
             
-        # 3x3 Box check
+        # Checks to see if the number is already in the same 3x3 box
         start_row, start_col = 3 * (row // 3), 3 * (col // 3)
         for i in range(start_row, start_row + 3):
             for j in range(start_col, start_col + 3):
                 if self.grid[i][j] == num:
                     return False
+        # If all checks are passed, the number placement is valid
         return True
     
+    # Checks if the board is completely filled
     def is_complete(self):
         return all(all(cell !=0 for cell in row) for row in self.grid)
 
+    # Prints the current state of the board to the terminal
     def display(self):
         for row in self.grid:
             print(row)
--- core/fsm.py
+++ core/fsm.py
@@ -1,21 +1,30 @@
 class State:
+    # Called when the FSM enters this state
     def enter(self): pass
+    
+    # Called continuously while this state is active
     def execute(self): pass
+    
+    # Called when transitioning out of this state
     def exit(self): pass
 
 class FSMManager:
     def __init__(self):
+        # Initialises the FSM with no current state
         self.state = None
 
+    # Switches FSM to a new state by calling exit() on the current state and then sets and enters a new state
     def set_state(self, state):
         if self.state:
             self.state.exit()
         self.state = state
         self.state.enter()
-        
+    
+    # Executes the current state's behaviour
     def update(self):
         if self.state:
             self.state.execute()
-            
+    
+    # Returns the current active state
     def get_state(self):
         return self.state
