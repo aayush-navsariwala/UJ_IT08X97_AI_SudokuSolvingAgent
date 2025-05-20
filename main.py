5c53570ea4e1e501d1c459d9fd69ad20dcfc0f44
aayush-navsariwala <aayush.nav@gmail.com>
2025-05-21 00:49:41 +0200

    Commit 38 - Added comments to main.py and test_image_input.py

--- main.py
+++ main.py
@@ -6,24 +6,32 @@ from core.states import InputState, ValidationState, SolvingState, WinState, Res
 from solver.backtracking import solve
 
 if __name__ == "__main__":
+    # Create an instance of the sudoku board
     board = SudokuBoard()
 
-    # Initializing the FSM
+    # Initialise the FSM
     fsm = FSMManager()
     
-    # Define the states and attach them to the FSM
+    # Instantiate and assign all of the FSM states
     fsm.input_state = InputState()
     fsm.validation_state = ValidationState(board, fsm)
     fsm.solving_state_class = SolvingState  
     fsm.win_state = WinState(board)
     fsm.reset_state = ResetState(board, fsm)
     
+    # Set the initial state of the FSM to input
     fsm.set_state(fsm.input_state)
 
-    # Launch GUI
+    # Create the main app window
     root = tk.Tk()
     root.title("Sudoku Solver AI (FSM-Based)")
     root.state('zoomed')  
+    
+    # Initialise the sudoku GUI with the board and FSM
     app = SudokuGUI(root, board, fsm)
+    
+    # Lets the FSM reference the GUI so it can update it
     fsm.gui = app
+    
+    # Run the app
     root.mainloop()
--- test_image_input.py
+++ test_image_input.py
@@ -1,5 +1,6 @@
 from utils.image_parser import image_to_grid
 
+# Test class for a sample image to test onto grid of GUI
 if __name__ == "__main__":
     path = "samples/test1.jpg"  
     grid = image_to_grid(path)
